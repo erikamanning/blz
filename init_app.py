d68ba57947bc6d4b085aca028896449941e1b457
gannon <erikamanning@protonmail.com>
2021-01-14 13:28:25 -0800

    created seed file

--- init_app.py
+++ init_app.py
@@ -3,23 +3,17 @@ from app import db
 db.drop_all()
 db.create_all()
 
-# python3 get_states.py; python3 get_positions.py; python3 get_parties.py; python3 get_members.py ; python3 get_bills.py ; python3 get_policy_areas.py ; python3 get_sessions.py
+'''
 
-# heroku run python3 init_app.py ; 
-# heroku run python3 get_states.py ;
-# heroku run python3 get_positions.py ;
-# heroku run python3 get_parties.py ;
-# heroku run python3 get_members.py ;
-# heroku run python3 get_bills.py ;
-# heroku run python3 get_policy_areas.py ;
-# heroku run python3 get_sessions.py ;
+    bash command:
+    python3 get_states.py; python3 get_positions.py; python3 get_parties.py; python3 get_members.py ; python3 get_bills.py ; python3 get_policy_areas.py ; python3 get_sessions.py
 
-
-# heroku run python3 init_app.py 
-# heroku run python3 get_states.py
-# heroku run python3 get_positions.py
-# heroku run python3 get_parties.py
-# heroku run python3 get_members.py
-# heroku run python3 get_bills.py
-# heroku run python3 get_policy_areas.py
-# heroku run python3 get_sessions.py
+'''
+import init_app 
+import get_states
+import get_positions
+import get_parties
+import get_members
+import get_bills
+import get_policy_areas
+import get_sessions
