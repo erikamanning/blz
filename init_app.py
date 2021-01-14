37a506e7fda384d23fa2a8af807b00a75f40549c
gannon <erikamanning@protonmail.com>
2021-01-14 10:35:31 -0800

    heroku changes

--- init_app.py
+++ init_app.py
@@ -4,3 +4,12 @@ db.drop_all()
 db.create_all()
 
 # python3 get_states.py; python3 get_positions.py; python3 get_parties.py; python3 get_members.py ; python3 get_bills.py ; python3 get_policy_areas.py ; python3 get_sessions.py
+
+# heroku run python3 init_app.py
+# heroku run python3 get_states.py
+# heroku run python3 get_positions.py
+# heroku run python3 get_parties.py
+# heroku run python3 get_members.py
+# heroku run  get_bills.py 
+# heroku run python3 get_policy_areas.py
+# heroku run python3 get_sessions.py
