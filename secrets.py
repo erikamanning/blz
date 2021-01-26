fd430a273d6cab888cf77551adbbf4609e865e45
gannon <erikamanning@protonmail.com>
2021-01-26 01:53:49 -0800

    fixed small import bug

--- secrets.py
+++ secrets.py
@@ -1,5 +1,5 @@
 import os
 
-API_SECRET_KEY = os.environ.get('SECRET_API_KEY', '##NOT A KEY##')
+API_SECRET_KEY = os.environ.get('SECRET-API-KEY', '##NOT A KEY##')
 FLASK_SECRET_KEY = os.environ.get('SECRET_KEY', '##NOT A KEY##')
 DATA_BASE_URI = os.environ.get('DATABASE_URL','postgresql:///blz')
